97104ec08d8af935f60409743d0e1004e890e848
roman.w <roman@odds.team>
2025-04-22 15:49:46 +0700

    create sftp service

--- pkg/sftp/sftp-service.go
+++ pkg/sftp/sftp-service.go
@@ -1,1 +1,15 @@
 package sftp
+
+import "fmt"
+
+type ISFTPService interface{}
+
+type sftpService struct{}
+
+func NewSFTPService() ISFTPService{
+	return &sftpService{}
+}
+
+func (service *sftpService) CloseClient(){
+	fmt.Println("close")
+}
--- pkg/sftp/sftp-service_test.go
+++ pkg/sftp/sftp-service_test.go
@@ -1,1 +1,23 @@
 package sftp
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewSFTPService(t *testing.T) {
+	// os.Setenv("SFTP_HOST", "test")
+	// os.Setenv("SFTP_PORT", "test")
+	// os.Setenv("SFTP_USERNAME", "test")
+	// os.Setenv("SFTP_PASSWORD", "test")
+	// os.Setenv("SFTP_BASEPATH", "test")
+	service := NewSFTPService()
+	v := reflect.Indirect(reflect.ValueOf(service))
+	for index := 0; index < v.NumField(); index++ {
+		if v.Type().Field(index).Name != "client" {
+			assert.False(t, v.Field(index).IsZero(), "Field %s is zero value", v.Type().Field(index).Name)
+		}
+	}
+}
