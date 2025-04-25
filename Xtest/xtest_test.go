7517046c34a1d1176a0305524faee29aacef3e47
Mubarak Damteh <might@odds.team>
2025-04-25 16:12:38 +0700

    [UPDATE] add integration test for person repository and modify ConnectMSSQL to return *gorm.DB

---
+++ Xtest/xtest_test.go
@@ -0,0 +1,35 @@
+package xtest
+
+import (
+	"os"
+	"poc-auto-read-ec/models"
+	"poc-auto-read-ec/pkg/person"
+	"testing"
+)
+
+func XTestInsertPersonToDB(t *testing.T) {
+
+	os.Setenv("DB_USER", "sa")
+	os.Setenv("DB_PASSWORD", "Maibok!In0ng")
+	os.Setenv("DB_HOST", "127.0.0.1")
+	os.Setenv("DB_PORT", "1433")
+	os.Setenv("DB_NAME", "Persons")
+	repo := person.NewPersonRepository()
+
+	testData := []models.GormPerson{
+		{
+			FirstName:   "Mighty",
+			LastName:    "Sanchez",
+			Email:       "Santomaru@fakeMail.com",
+			PhoneNumber: "+66999999999",
+			DateOfBirth: "1990-01-01",
+			Address:     "123 Main St, City, Country",
+		},
+	}
+
+	err := repo.InsertPersonToDB(testData)
+	if err != nil {
+		t.Errorf("Expected no error, got 4444 %v", err)
+	}
+
+}
