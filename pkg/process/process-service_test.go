303b3789db7239af7525db067a2573586a1d5944
Donatsuu <donatsu@odds.team>
2025-04-25 15:34:17 +0700

    [UPDATE] add unit test for process-service for function ProcessAutoReadEC

--- pkg/process/process-service_test.go
+++ pkg/process/process-service_test.go
@@ -1,10 +1,11 @@
 package process
 
 import (
+	"github.com/stretchr/testify/assert"
+	"poc-auto-read-ec/internal/fake"
+	"poc-auto-read-ec/models"
 	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestNewProcessService(t *testing.T) {
@@ -15,4 +16,18 @@ func TestNewProcessService(t *testing.T) {
 	for index := 0; index < v.NumField(); index++ {
 		assert.False(t, v.Field(index).IsZero(), "Field %s is zero value", v.Type().Field(index).Name)
 	}
+}
+
+func TestCallingConnectClient(t *testing.T) {
+	mockSFTPService := &fake.MockSFTPService{}
+	testFiles := []models.CSVRawFile{
+		{FileName: "test-list.csv", RawFile: []byte{72, 101, 108, 108, 111}},
+		{FileName: "test-list-2.csv", RawFile: []byte{72, 101, 108, 108, 108}},
+	}
+	mockSFTPService.Mock.On("GetAllCSVFile").Return(testFiles)
+	service := &processService{
+		sftpService: mockSFTPService,
+	}
+
+	service.ProcessAutoReadEC()
 }
